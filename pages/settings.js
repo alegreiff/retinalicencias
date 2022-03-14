21f75aabe69dd692cc90d9d2c5be2aab464402e9
Jaime de Greiff <alegreiff@gmail.com>
2022-03-14 15:45:09 -0500

    set up

--- pages/settings.js
+++ pages/settings.js
@@ -32,6 +32,7 @@ import "react-datepicker/dist/react-datepicker.css";
 
 import { registerLocale, setDefaultLocale } from "react-datepicker";
 import es from "date-fns/locale/es";
+
 registerLocale("es", es);
 
 const PageSettings = (props) => {
